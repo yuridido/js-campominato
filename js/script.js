9830d4d51fc6e4b31b318b6a9edf27f0c2865ade
yuridido <yuridido@yahoo.it>
2020-07-20 15:19:37 +0200

    genera numeri pc

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati
+// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all'inizio il software richiede anche una difficoltà all'utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+// Consigli del giorno: :party_wizard:
+// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+// Ad esempio:
+// Di cosa ho bisogno per generare i numeri?
+// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+// Proviamo prima con pochi numeri, inserire 84 numeri ogni volta potrebbe essere un po’ scocciante :wink:
+// Le validazioni e i controlli possiamo farli anche in un secondo momento.
+// Ricordatevi che se non sappiamo quante volte dobbiamo fare una cosa ci serve…
+
+// CREO 2 ARRAY
+var numeriPc = [];
+var numeriUtente = [];
+var difficolta = 100;
+// CREO 16 NUMERI CASUALI
+var i = 1;
+var massimo = difficolta - 16;
+while (numeriPc.length < 16) {
+    numeriPc.push(random(1, massimo));
+}
+console.log(numeriPc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNZIONI
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
